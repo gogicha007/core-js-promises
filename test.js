528f7434335b08864f10b42cb244c1458c8f5c4f
gogicha007 <gogicha@gmail.com>
2024-04-05 17:30:50 +0400

    second task done

--- test.js
+++ test.js
@@ -1,6 +1,9 @@
-function getPromise(number) {
-    return new Promise((resolve, reject) => {
-      if (number < 0) reject();
-      resolve();
-    });
-  }
+function getPromiseResult(source) {
+  return source.then(() => 'success').catch(() => 'fail');
+}
+
+const resolver = (number) =>
+  number % 2 === 0 ? Promise.resolve() : Promise.reject();
+
+console.log(getPromiseResult(resolver(2)));
+console.log(getPromiseResult(resolver(9)));
